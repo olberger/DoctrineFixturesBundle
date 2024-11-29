42a0cbe8c31ad29fd2be254f81a5141e79e391f6
Grégoire Paris <postmaster@greg0ire.fr>
2024-11-29 09:01:12 +0100

    Use proper namespace

--- tests/Purger/ORMPurgerFactoryTest.php
+++ tests/Purger/ORMPurgerFactoryTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Purger;
+namespace Doctrine\Bundle\FixturesBundle\Tests\Purger;
 
 use Doctrine\Bundle\FixturesBundle\Purger\ORMPurgerFactory;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
